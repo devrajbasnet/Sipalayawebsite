09351266d44ea7a1a4a8bd2120ce8537206e9842
Dev Raj Basnet <67828453+jrdevubasnet@users.noreply.github.com>
2024-04-25 21:57:26 +0545

    FormRegister.jsx

--- src/Components/Navbar/FormRegister.jsx
+++ src/Components/Navbar/FormRegister.jsx
@@ -1,6 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 const FormRegister = () => {
+  const [fullName, setFullName] = useState('');
+  const [MobileNumber, setMobileNumber] = useState('');
+  const [college, setCollege] = useState('');
+  const [qualification, setQualification] = useState('');
+  const [interest, setInterest] = useState('');
+  const [programmingSkills, setProgrammingSkills] = useState('');
+  const [schedule, setSchedule] = useState('');
+  const [classs, setClass]=useState('');
+  const [classType, setClassType] = useState('');
+  const [requireInternship, setRequireInternship] = useState('');
+
+  const handleFullNameChange = (event) => {
+    const input = event.target.value;
+    const sanitizedInput = input.replace(/[^a-zA-Z\s]/g, '');
+    setFullName(sanitizedInput);
+  };
+
+  const handleMobileNumberChange = (event) => {
+    const input = event.target.value;
+    const sanitizedInput = input.replace(/\D/g, '');
+    setMobileNumber(sanitizedInput);
+  };
+  
+
+  const handleQualificationChange = (event) => {
+    setQualification(event.target.value);
+  };
+
+  const handleInterestChange = (event) => {
+    const input = event.target.value;
+   
+    const sanitizedInput = input.replace(/[^a-zA-Z\s]/g, '');
+    setInterest(sanitizedInput);
+  };
+  const handleProgrammingSkillsChange = (event) => {
+    setProgrammingSkills(event.target.value);
+  };
+  
+  const handleScheduleChange = (event) => {
+    setSchedule(event.target.value);
+  };
+  const handleClassTypeChange = (event) => {
+    setClassType(event.target.value);
+  };
+  const handleRequireInternshipChange = (event) => {
+    setRequireInternship(event.target.value);
+  };
+
+
+
   return (
     <div className="form">
       <p>Fields marked with an * are required</p>
@@ -8,13 +58,15 @@ const FormRegister = () => {
       <form>
         <div className="form-field">
           <label htmlFor="fullName">Full Name *</label>
-          <input type="text" id="fullName" name="fullName" placeholder="Full Name" />
+          <input type="text" id="fullName" name="fullName" placeholder="Full Name"  value={fullName} 
+            onChange={handleFullNameChange}/>
           <p className="character-count">3 of 3 Character(s) left</p>
         </div>
         
         <div className="form-field">
           <label htmlFor="mobileNumber">Mobile Number *</label>
-          <input type="tel" id="mobileNumber" name="mobileNumber" placeholder="Mobile Number" />
+          <input type="tel" id="mobileNumber" name="mobileNumber" placeholder="Mobile Number" value={MobileNumber} 
+            onChange={handleMobileNumberChange}/>
           <p className="character-count">10 of 10 Character(s) left</p>
         </div>
         
@@ -29,7 +81,18 @@ const FormRegister = () => {
         </div>
         <div className="form-field">
         <label htmlFor="Qualification">Qualification *</label>
-            <input type="text" id="qualification" name="qualification" placeholder="Qualification"/>
+        <select>
+            <input type="text" id="qualification" name="qualification"  value={qualification} 
+            onChange={handleQualificationChange}/>
+             
+            <option value="">Select Qualification</option>
+            <option value="SEE">SEE</option>
+            <option value="+2">+2</option>
+            <option value="Bachelor">Bachelor</option>
+            <option value="Master">Master</option>
+            <option value="PhD">PhD</option>
+            <option value="Other">Other</option>
+          </select>
         </div>
          <div className="form-field">
          <label htmlFor="Address">Address *</label>
@@ -38,7 +101,9 @@ const FormRegister = () => {
          <div className="form-field">
   <label>Your Programming Skills? *</label>
   <div className="radio-options">
-    <input type="radio" id="none" name="programmingSkills" value="None" />
+    <input type="radio" id="none" name="programmingSkills" value="None" 
+    checked={programmingSkills === 'Basic'} 
+   onChange={handleProgrammingSkillsChange}  />
     <label htmlFor="none">None</label>
     <input type="radio" id="basic" name="programmingSkills" value="Basic" />
     <label htmlFor="basic">Basic</label>
@@ -52,34 +117,53 @@ const FormRegister = () => {
 </div>
 <div className="form-field">
          <label htmlFor="interest">I am Interest in *</label>
-            <input type="text" id="interest" name="Interset" placeholder="Python"/>
+         <select>
+            <input type="text" id="interest" name="interset"value={interest} 
+            onChange={handleInterestChange}/>
+              
+              <option value="">Select Qualification</option>
+            <option value="Python and Django">Python and Django</option>
+            <option value="MERN">MERN</option>
+            <option value="C">C</option>
+            <option value="Java">Java</option>
+            <option value="Flutter">Flutter</option>
+            <option value="Other">Other</option>
+          </select>
+            
          </div>
          <div className="form-field">
           <label>Your Scheduule *</label>
           <div className="radio-options">
-            <input type="radio" id="none" name="Morning" value="Morning"/>
-            <label htmlFor="Morning">Morning</label> 
-            <input type="radio" id="none" name="Day" value="Day"/>
+            <input type="radio" id="Day" name="schedule" value="Day"checked={schedule === 'Day'} 
+              onChange={handleScheduleChange} />
             <label htmlFor="Day">Day</label> 
-            <input type="radio" id="none" name="Evening" value="Evening"/>
+            <input type="radio" id="Morning" name="schedule" value="Morning" checked={schedule === 'Morning'} 
+              onChange={handleScheduleChange} />
+            <label htmlFor="Morning">Morning</label> 
+            <input type="radio" id="Evening" name="schedule" value="Evening"checked={schedule === 'Evening'} 
+              onChange={handleScheduleChange} />
             <label htmlFor="Evening">Evening</label> <br/>
           </div>
          </div>
          <div className="form-field">
           <label>Physical class(Koteshwor,ktm)or OnlineClass *</label>
           <div className="radio-options">
-            <input type="radio" id="none" name="Online" value="Online"/>
+            <input type="radio" id="online" name="classType" value="Online"  checked={classType === 'Online'} 
+              onChange={handleClassTypeChange} />
             <label htmlFor="Online">Online</label> 
-            <input type="radio" id="none" name="Physical" value="Physical"/>
+            <input type="radio" id="physical" name="classType" value="Physical"  checked={classType === 'Physical'} 
+              onChange={handleClassTypeChange} />
             <label htmlFor="Physical">Physical</label> 
             </div>
             </div>
             <div className="form-field">
           <label>Do you require internship after training? *</label>
           <div className="radio-options">
-            <input type="radio" id="none" name="Yes" value="Yes"/>
+            <input type="radio" id="yes" name="requireInternship" value="Yes" checked={requireInternship === 'Yes'} 
+              onChange={handleRequireInternshipChange}/>
             <label htmlFor="yes">Yes</label> 
-            <input type="radio" id="none" name="No" value="No"/>
+            <input type="radio" id="no" name="requireInternship" value="No" checked={requireInternship === 'No'} 
+              onChange={handleRequireInternshipChange} />
             <label htmlFor="No">No</label> 
             </div>
             </div>
@@ -87,7 +171,6 @@ const FormRegister = () => {
           <label htmlFor="queries">Any Queries?</label>
           <textarea id="queries" name="queries" placeholder="Any Queries?" rows="4" />
         </div>
-        
         <button type="submit">Register</button>
       </form>
     </div>
